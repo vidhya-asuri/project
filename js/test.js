1a110fb28db77cc49aa6d79a6bcfa89e8fbfe4e5
Vidhya Asuri <sasuri@sstire.com>
2019-05-31 15:51:48 -0400

    add form to enter new purchase order; add table to display purchase orders

--- js/test.js
+++ js/test.js
@@ -8,57 +8,155 @@
 
 // The collection of todos is backed by *localStorage* instead of a remote
 // server.
-
-var Task = Backbone.Model.extend({
+var PO = Backbone.Model.extend({
 	defaults:{
-		  name: 'learn backbone',
-		  date: '1/1/2019'
+		  name: 'default name',
+		  date: '1/1/2000',
+		  email: 'test@sstire.com',
+		  location: 1,
+		  amount: 0.0,
+		  status: 1,
+		  vendor: 'default vendor',
+		  description: 'default description'
 	},
-	url:"/tasks/new"
+	url:"/pos"	
 });
 
+var POs = Backbone.Collection.extend({
+	model: PO,
+	url: "/pos"
+});
 
-// Create our global collection of **Todos**.
-var task = new Task();
-console.log(task.get('name'));
-console.log(task.get('date'));
+var poModel = new PO;
+var poCollection = new POs;
+// Get model with ID = 11 from database
+// read → GET   /collection[/id]
 
-
-var taskV = new Task({ name: "new task 2 May 24", date: '5/24/2019 ' });
-taskV.save({}, {
-    success: function (model, response, options) {
-    	console.log("model");
-    	console.log(model);
-    	
-        console.log("The model has been saved to the server");
+/*poCollection.fetch({
+    success: function(collection, response, options) 
+    {
+        alert(JSON.stringify(response));
     },
-    error: function (model, xhr, options) {
-        console.log("Something went wrong while saving the model");
+    error: function(collection, response, options)
+    {
+        alert(JSON.stringify(response));     
     }
+});*/
+// collection.sync(method, collection, [options])
+
+// View for table listing collection of purchase orders
+var POsView = Backbone.View.extend({
+	el: '#purchaseOrdersListDiv',
+	initialize: function(){
+    	// Fetch all the purchase orders from database and display in the
+    	// purchase orders table.
+    	var pos = new POs;
+    	pos.fetch({
+		        success: function(collection, response, options) 
+		        {
+		            // alert(JSON.stringify(response));
+		        	  var purchaseOrdersTable = new Tabulator("#purchaseOrdersList", {
+		      		  	height:'90vh', 
+		      		  	layout:"fitDataFill",
+		      		    rowClick:function(e, row){
+		      		    },
+		      			columns:[ 
+		      				    {title:"ID ", field:"po_id" , headerFilter:"input"},
+		      				    {title:"Po Number", field:"po_num",  headerFilter:"input"},
+		      				    {title:"Date", field:"date", headerFilter:"input" },
+		      				    {title:"Name", field:"name"},
+		      				    {title:"Email", field:"email"},
+		      				    {title:"Vendor", field:"vendor", headerFilter:"input"},
+		      				    {title:"Description", field:"description"},
+		      				    {title:"Amount ", field:"amount"},
+		      				    {title:"Purchase location", field:"purchase_location"},
+		      				    {title:"Status", field:"status"}]
+		        	  });	 
+		          	  
+		        	  purchaseOrdersTable.setData(response);
+		        },
+		        error: function(collection, response, options)
+		        {
+		            alert(JSON.stringify(response));     
+		        }
+       });
+	}
 });
 
+    	
+var posView = new POsView;
 
-//var task1 = new Task();
-//task1.set('name', 'task1 text');
-//task1.set('date', '2/1/2019');
-//console.log(task1.get('name'));
-//console.log(task1.get('date'));
-//
-var Tasks = Backbone.Collection.extend({
-	model: Task,
-	url: "/tasks"
+var POView = Backbone.View.extend({
+	el: '#addPOFormDiv',
+    events: 
+    {
+    	"submit #addPOForm" : "addPO"     
+    }, 
+    initialize: function(){
+
+    },
+    addPO :  function(e)
+    {  
+        e.preventDefault();
+    	console.log("POView::save function called ");
+    	
+        var name = $('#name').val();
+        var email = $('#email').val();  
+        var vendor = $('#vendor').val(); 
+        var description = $('#description').val(); 
+        var amount = $('#amount').val(); 
+        var location = $('#location').val(); 
+
+        var formData = {
+            name : name, 
+            email : email,  
+            vendor : vendor,
+            description : description,
+            amount : amount,
+            location : location
+        };
+
+        var po = new PO();              
+        po.set('name', name);
+        po.set('email', email);
+        po.set('vendor', vendor);
+        po.set('description', description);
+        po.set('amount', amount);
+        po.set('location', location);
+        
+        var pos = new POs();
+        
+        var newPurchaseOrder  = pos.create(po);
+        console.log(newPurchaseOrder);
+       
+        // read (GET) /pos
+        
+/*        pos.fetch({
+	        success: function(collection, response, options) 
+	        {
+	            alert(JSON.stringify(response));
+	        },
+	        error: function(collection, response, options)
+	        {
+	            alert(JSON.stringify(response));     
+	        }
+        });
+*/        
+        /*
+        The default sync handler maps CRUD to REST like so:
+
+        	create → POST   /collection
+        	read → GET   /collection[/id]
+        	update → PUT   /collection/id
+        	patch → PATCH   /collection/id
+        	delete → DELETE   /collection/id
+        */
+        
+     
+        
+    }
+
 });
-//
-//
-var tasksCollection = new Tasks();
-//tasksCollection.add(task);
-//tasksCollection.add(task1);
-//
-tasksCollection.fetch();
 
-console.log("tasks in db");
-tasksCollection.slice(0, tasksCollection.length ); 
+var poView = new POView();
 
-  
-  
-  
--- src/api/api.php
+++ src/api/api.php
@@ -4,54 +4,81 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
-
-
-
 $app = new \Slim\App;
+
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
-
     return $response;
 });
 
-$app->get('/tasks', "getAllTasks" );
 
-$app->post('/tasks', "getAllTasks" );
-
-
-$app->post('/tasks/new',"save");	
+$app->get('/pos', "getAllPOs" );
+$app->post('/pos', "create" );
+$app->get('/pos/{po_id}', "read" );
 
 
-
+/*****
+ * https://stackoverflow.com/questions/630453/put-vs-post-in-rest
+ * 
+ * https://stackoverflow.com/questions/18504235/understand-backbone-js-rest-calls
+ * 
+ * https://www.mike-miles.com/blog/reading-backbonejs-post-variables-php
+ * 
+ * https://kleopetrov.me/2015/12/10/Backbone-collections/
+ * 
+ */
 $app->run();
 
 
-function save() {
-	//https://coderwall.com/p/vwvy_a/retrieving-post-data-from-backbone-js-in-php
-	$var = json_decode(file_get_contents('php://input'), true);
-	// echo  '{"post data ": ' . json_encode($var) . '}';
-	$sql = "insert into tasks (name, date) values ( '".  $var['name'] . "' , '" . $var['date'] . "' )";
- 	try {
+function read(Request $request, Response $response, array $args) {
+	$poNumber = $args['po_id'];
+
+	$sql = "select * FROM po_test where po_id = " . $poNumber;
+	try {
 		$db = getConnection();
+		// echo "connection successful";
 		$stmt = $db->query($sql);
+		$po = $stmt->fetchAll(PDO::FETCH_OBJ);
 		$db = null;
-		echo '{"sql ": ' . json_encode($sql) . '}';
+		echo '{"app::get /pos/{poNumber}": ' . json_encode($po) . '}';
 	} catch(PDOException $e) {
 		echo '{"error":{"text":'. $e->getMessage() .'}}';
 	}
 	
 }
 
-function getAllTasks() {
-	$sql = "select * FROM tasks ";
+// insert into po_test ( po_num,date, name, email,vendor, description, amount, purchase_location, status)
+// values ('LEX1234', 20190528, 'Vidhya', 'sasuri@sstire.com', 'Dell', ' laptop', 600.55 ,1, 1);
+function create() {
+
+	$formData = json_decode(file_get_contents('php://input'), true);
+	$sql = "insert into po_test ( po_num,name, date, email,vendor, description, amount, purchase_location, status)
+            values ( 'Test_Po_Number', '".  $formData['name'] . "' , " .date("Ymd") ." ,'" . $formData['email'] . "' , '" . $formData['vendor'] . "' , '" . $formData['description'] . "' , '" . $formData['amount'] . "' , " . $formData['location'] . ",1" . ")";
+	try {
+		$db = getConnection();
+		$stmt = $db->query($sql);
+		// $saveResult = $stmt->fetchAll(PDO::FETCH_OBJ);
+		$db = null;
+		echo '{"save result ": ' . json_encode($stmt) . '}';
+	} catch(PDOException $e) {
+		echo '{"error":{"text":'. $e->getMessage() .'}}';
+	}
+// 	echo json_encode($sql);
+
+}
+
+
+function getAllPOs() {
+	$sql = "select * FROM po_test ";
 	try {
 		$db = getConnection();
 		// echo "connection successful";
 		$stmt = $db->query($sql);
 		$pos = $stmt->fetchAll(PDO::FETCH_OBJ);
 		$db = null;
-		echo '{"tasks ": ' . json_encode($pos) . '}';
+		//echo '{"pos ": ' . json_encode($pos) . '}';
+		echo json_encode($pos);
 	} catch(PDOException $e) {
 		echo '{"error":{"text":'. $e->getMessage() .'}}';
 	}
